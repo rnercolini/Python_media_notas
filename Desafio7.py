675b8b2d0406288effc97f4a0a569f5a4175af96
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 14:05:53 -0300

    Add files via upload

---
+++ Desafio7.py
@@ -0,0 +1,5 @@
+# Calcula a média de dois números
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2)/2
+print('A média entre {0} e {1} é {2:.1f}'.format(n1, n2, m))
